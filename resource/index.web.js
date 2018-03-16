8931e28e9c2bd53e7354595734707fdd56f507d9
anvu-trustcircle <an.vu@trustcircle.com>
2018-03-16 10:46:25 +0700

    remove resource for test git ignore

--- resource/index.web.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { AppRegistry } from 'react-native';
-import App from './src/App';
-
-AppRegistry.registerComponent('AppGenerate', () => App);/nAppRegistry.runApplication('AppGenerate', {/n  initialProps: {},
-  rootTag: document.getElementById('react-app')
-});
